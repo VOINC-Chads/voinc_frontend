f71402edfe67d38201bca890735f81284a10eec1
Du Duong <khaidu2002@gmail.com>
2023-04-04 14:55:41 -0500

    side_by_side

--- voinc_fe/src/App.js
+++ voinc_fe/src/App.js
@@ -9,16 +9,24 @@ import 'ace-builds/src-noconflict/theme-monokai';
 import "ace-builds/src-noconflict/ext-language_tools"
 
 function App() {
-  var starter = `def function(data):
+  var starter = `def map_function(data):
   # enter function here
-  print("VOINC")`
-  const [code, setCode] = useState(starter);
+  for i in data:
+    map_to_inter(i)`
+
+  var reduceStarter = `def reduce_function(intermediate):
+  # enter reduce function here
+  for key, value in intermediate.items():
+    reduce_to_final(key, value)
+  `
+  const [mapCode, setMapCode] = useState(starter);
+  const [reduceCode, setReduceCode] = useState(reduceStarter);
   const [output, setOutput] = useState('');
   const runCode = async () => {
     const response = await fetch('/run-python', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ code })
+      body: JSON.stringify({ mapCode, reduceCode })
     });
     const result = await response.json();
     setOutput(result.output);
@@ -27,14 +35,18 @@ function App() {
   return (
     <div className="container vh-100">
       <div>
-        <h1 className='d-flex text-muted font-monospace justify-content-center mt-5 mb-3 aligned-content-center'>VOINC EDITOR</h1>
+        <h1 className='d-flex text-dark font-monospace justify-content-center mt-5 mb-3 aligned-content-center'>VOINC EDITOR</h1>
+        <div  className='row'>
+          <h3 className='col-6 d-flex text-muted font-monospace justify-content-center'>Map Section</h3>
+          <h3 className='col-6 d-flex text-muted font-monospace justify-content-center'>Reduce Section</h3>
+        </div>
       </div>
       <div id="editor" className='d-flex justify-content-center aligned-self-center'>
         <AceEditor
           mode="python"
           theme="monokai"
-          value={code}
-          onChange={(value) => setCode(value)}
+          value={mapCode}
+          onChange={(value) => setMapCode(value)}
           name="editor"
           editorProps={{ $blockScrolling: true }}
           height="60vh"
@@ -49,12 +61,36 @@ function App() {
             showLineNumbers: true,
             tabSize: 2,
           }}
+          className='mr-2'
+        />
+         <AceEditor
+          mode="python"
+          theme="monokai"
+          value={reduceCode}
+          onChange={(value) => setReduceCode(value)}
+          name="editor"
+          editorProps={{ $blockScrolling: true }}
+          height="60vh"
+          width="60%"
+          fontSize={16}
+          showPrintMargin={true}
+          highlightActiveLine={true}
+          setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+            enableSnippets: false,
+            showLineNumbers: true,
+            tabSize: 2,
+          }}
+          className='ml-2'
+
         />
       
       </div>
       <div className='d-flex justify-content-center p-4 aligned-self-center'>
         <button className='btn btn-success px-5 py-2' onClick={runCode}>SEND CODE TO EXECUTE</button>
       </div>
+      <h1>{output}</h1>
     </div>
 
   );
